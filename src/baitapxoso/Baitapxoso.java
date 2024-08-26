aa2d897077a313e650d4302b2ca665feaf7d8f47
Tu's lenovo <Tu's lenovo@TU-LENOVO>
2024-08-26 21:20:06 +0700

    xoso

---
+++ src/baitapxoso/Baitapxoso.java
@@ -0,0 +1,12 @@
+package baitapxoso;
+import java.util.Random;
+
+public class Baitapxoso {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int xoso = rand.nextInt(101);
+        System.out.println("Ket qua xo so cua ban la: "+ xoso);
+    }
+    
+}
