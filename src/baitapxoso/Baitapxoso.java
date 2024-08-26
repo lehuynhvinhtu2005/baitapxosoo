d81336aef9b89902e7e6e238f52ff95a9d35ddc7
lehuynhvinhtu2005 <151424780+lehuynhvinhtu2005@users.noreply.github.com>
2024-08-26 21:54:55 +0700

    Baitapxoso.java

--- src/baitapxoso/Baitapxoso.java
+++ src/baitapxoso/Baitapxoso.java
@@ -5,7 +5,7 @@ public class Baitapxoso {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int xoso = rand.nextInt(101);
+        int xoso = rand.nextInt(100) + 1;
         System.out.println("Ket qua xo so cua ban la: "+ xoso);
     }
     
